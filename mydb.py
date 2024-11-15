e78b8a988fabedafa1b8c9c4e3e08ee0260ef732
Manuja100 <manujamunasinghe1@gmail.com>
2024-11-15 22:37:56 +0530

    Initial Commit

---
+++ mydb.py
@@ -0,0 +1,15 @@
+import mysql.connector
+
+database = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    passwd = '21323966'
+)
+
+#prepare a cursor object
+cursorObject = database.cursor()
+
+#creating the db
+cursorObject.execute("CREATE DATABASE crm_django")
+
+print("Database Created")
